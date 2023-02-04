9d55ee8dec6d6443845f372fef49157404d02694
Kamran <311.kamran@gmail.com>
2023-02-04 08:42:17 +0400

    Complete Insert Books

--- Ado.Net_Homework2/Views/AddBookWindow.xaml.cs
+++ Ado.Net_Homework2/Views/AddBookWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +27,103 @@ public partial class AddBookWindow : Window
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
+        using (var conn = new SqlConnection())
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["myConnString"].ConnectionString;
+            DataSet set = new DataSet();
+
+
+            conn.Open();
+
+
+            SqlCommand sqlCommand = new SqlCommand("ShowAllBooks", conn);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            da.SelectCommand = sqlCommand;
+            da.Fill(set, "Books");
+
+
+            var paramId = new SqlParameter();
+            paramId.ParameterName = "@Id";
+            paramId.SqlDbType = SqlDbType.Int;
+            paramId.Value = id_txt.Text;
+
+
+            var paramName = new SqlParameter();
+            paramName.ParameterName = "@Name";
+            paramName.SqlDbType = SqlDbType.NVarChar;
+            paramName.Value = name_txt.Text;
+
+
+            var paramPages = new SqlParameter();
+            paramPages.ParameterName = "@Pages";
+            paramPages.SqlDbType = SqlDbType.Int;
+            paramPages.Value = pages_txt.Text;
+
+            var paramYear = new SqlParameter();
+            paramYear.ParameterName = "@YearPress";
+            paramYear.SqlDbType = SqlDbType.Int;
+            paramYear.Value = yearpress_txt.Text;
+
+            var paramThemes = new SqlParameter();
+            paramThemes.ParameterName = "@Id_Themes";
+            paramThemes.SqlDbType = SqlDbType.Int;
+            paramThemes.Value = idThemes_txt.Text;
+
+            var paramCategory = new SqlParameter();
+            paramCategory.ParameterName = "@Id_Category";
+            paramCategory.SqlDbType = SqlDbType.Int;
+            paramCategory.Value = idCategory_txt.Text;
+
+
+            var paramAuthor = new SqlParameter();
+            paramAuthor.ParameterName = "@Id_Author";
+            paramAuthor.SqlDbType = SqlDbType.Int;
+            paramAuthor.Value = idAuthor_txt.Text;
+
+            var paramPress = new SqlParameter();
+            paramPress.ParameterName = "@Id_Press";
+            paramPress.SqlDbType = SqlDbType.Int;
+            paramPress.Value = idPress_txt.Text;
+
+            var paramComment = new SqlParameter();
+            paramComment.ParameterName = "@Comment";
+            paramComment.SqlDbType = SqlDbType.NVarChar;
+            paramComment.Value = comment_txt.Text;
+
+            var paramQuantity = new SqlParameter();
+            paramQuantity.ParameterName = "@Quantity";
+            paramQuantity.SqlDbType = SqlDbType.Int;
+            paramQuantity.Value = quantity_txt.Text;
+
+            using (sqlCommand = new SqlCommand("InsertBook", conn))
+            {
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.Add(paramId);
+                sqlCommand.Parameters.Add(paramName);
+                sqlCommand.Parameters.Add(paramPages);
+                sqlCommand.Parameters.Add(paramYear);
+                sqlCommand.Parameters.Add(paramThemes);
+                sqlCommand.Parameters.Add(paramCategory);
+                sqlCommand.Parameters.Add(paramAuthor);
+                sqlCommand.Parameters.Add(paramPress);
+                sqlCommand.Parameters.Add(paramComment);
+                sqlCommand.Parameters.Add(paramQuantity);
+
+
+                da.InsertCommand = sqlCommand;
+
+                da.Update(set, "Books");
+                da.InsertCommand.ExecuteNonQuery();
+
+                da.Fill(set, "Books");
+                MessageBox.Show("Succesfully Added");
+            }
+
+            DialogResult = true;
+        }
 
     }
 }
