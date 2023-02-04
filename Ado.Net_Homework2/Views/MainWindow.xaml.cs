a505e1ed35e1286591efeeba7b46252df8a11aa4
Kamran <311.kamran@gmail.com>
2023-02-04 08:29:53 +0400

    Complete Show All Books & Delete Books

--- Ado.Net_Homework2/Views/MainWindow.xaml.cs
+++ Ado.Net_Homework2/Views/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Ado.Net_Homework2.Views;
+﻿using Ado.Net_Homework2.Models;
+using Ado.Net_Homework2.Views;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +24,14 @@ namespace Ado.Net_Homework2;
 
 public partial class MainWindow : Window
 {
+
+    DataTable table;
+    SqlDataReader reader;
+    SqlDataAdapter da;
+    DataSet set;
+
+
+
     public MainWindow()
     {
         InitializeComponent();
@@ -39,11 +51,66 @@ public partial class MainWindow : Window
 
     private void Delete_Click_1(object sender, RoutedEventArgs e)
     {
+        var obj = myDataGrid.SelectedItem;
+        var nese = obj as DataRowView;
+        var book = nese!.Row.ItemArray;
 
+        using (var conn = new SqlConnection())
+        {
+            da = new SqlDataAdapter();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["myConnString"].ConnectionString;
+            conn.Open();
+            set = new DataSet();
+
+            SqlCommand sqlCommand = new SqlCommand("ShowAllBooks", conn);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            da.SelectCommand = sqlCommand;
+            da.Fill(set, "Books");
+
+
+
+            sqlCommand = new SqlCommand("DeleteBooks", conn);
+
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+
+            sqlCommand.Parameters.Add(new SqlParameter
+            {
+                DbType = DbType.Int32,
+                ParameterName = "@booksId",
+                Value = book[0]
+            });
+
+            da.UpdateCommand = sqlCommand;
+
+            da.Update(set, "Books");
+            da.UpdateCommand.ExecuteNonQuery();
+
+            da.Fill(set, "Books");
+        }
     }
+
+
 
     private void ShowAll_Click(object sender, RoutedEventArgs e)
     {
 
+        using (var conn = new SqlConnection())
+        {
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["myConnString"].ConnectionString;
+
+            da = new SqlDataAdapter();
+            conn.Open();
+            set = new DataSet();
+
+            SqlCommand command = new SqlCommand("ShowAllBooks", conn);
+            command.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand = command;
+
+            da.Fill(set, "AllBooks");
+            myDataGrid.ItemsSource = set.Tables[0].DefaultView;
+        }
     }
+
 }
